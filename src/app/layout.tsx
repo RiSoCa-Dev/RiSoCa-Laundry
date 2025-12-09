16a86076f16bd63c8c25327d75f8eb30f210fd0a
risocode <risoca.code@gmail.com>
2025-12-09 22:33:16 +0800

    Update icon paths in layout.tsx to use new directory structure for improved organization

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,13 +15,12 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
-      { rel: 'icon', url: '/favicon.ico', sizes: 'any' },
-      { rel: 'icon', url: '/icons/icon-192x192.svg', type: 'image/svg+xml' },
-      { rel: 'icon', url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
-      { rel: 'icon', url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { rel: 'icon', url: '/icons/favicon.ico', sizes: 'any' },
+      { rel: 'icon', url: '/icons/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { rel: 'icon', url: '/icons/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
     ],
     apple: [
-      { url: '/apple-touch-icon.png', sizes: '180x180' },
+      { url: '/icons/apple-touch-icon.png', sizes: '180x180' },
     ],
   },
   themeColor: '#6d28d9',
@@ -44,11 +43,10 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
         <link rel="manifest" href="/manifest.json" />
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="icon" type="image/svg+xml" href="/icons/icon-192x192.svg" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" href="/icons/favicon.ico" sizes="any" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+        <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
       </head>
       <body className="font-body antialiased flex flex-col h-screen overflow-hidden">
         {children}
